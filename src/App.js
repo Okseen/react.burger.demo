1ef390cae254bd9f9d1026078f0a6f4bcddea19b
Okseen <Okseen01@mail.ru>
2018-01-24 23:15:48 +0200

    -

--- src/App.js
+++ src/App.js
@@ -8,7 +8,20 @@ class App extends Component {
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age: 26},
-    ]
+    ],
+    otherState: 'some other value'
+  }
+
+  swithcNameHandler = () => {
+    //console.log('Was clicked!');
+    //this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        {name: "Cucumber", age: 30},
+        {name: 'Manu', age: 29},
+        {name: 'Stephanie', age: 26},
+      ]
+    });
   }
 
   render() {
@@ -16,7 +29,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I a'm React App</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
+        <button onClick={this.swithcNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
